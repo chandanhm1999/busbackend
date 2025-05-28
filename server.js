969f63b606bec8c97b84de29c89b928e3f38dfb7
chandanhm1999 <chandanchanduhm4@gmail.com>
2025-05-28 18:27:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+// Helper function to read JSON file
+function readJson(fileName) {
+  const filePath = path.join(__dirname, "data", fileName);
+  const data = fs.readFileSync(filePath, "utf8");
+  return JSON.parse(data);
+}
+
+// Endpoints
+app.get("/routes", (req, res) => {
+  const routes = readJson("routes.json");
+  res.json(routes);
+});
+
+app.get("/stops", (req, res) => {
+  const stops = readJson("stops.json");
+  res.json(stops);
+});
+
+app.get("/trips", (req, res) => {
+  const trips = readJson("trips.json");
+  res.json(trips);
+});
+
+app.get("/buses", (req, res) => {
+  const buses = readJson("buses.json");
+  res.json(buses);
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend server running on http://localhost:${PORT}`);
+});
